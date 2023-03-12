7989bb7b39a43900afc662ea8ad5dc4da5aa94a2
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-03-12 01:44:05 -0600

    complete of objects lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name : "Sam",
+    streetAddress: "11 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, value){
+    employee[name] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newisEmployee = {...employee};
+    delete newisEmployee[key];
+    return newisEmployee;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee[name];
+    return employee;
+}
